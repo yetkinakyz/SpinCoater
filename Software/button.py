009a5ff31eed2d9300b6920804b43c7bb9bcb51d
Yetkin Akyüz <yetkin.akyz@gmail.com>
2020-11-18 19:24:43 +0300

    button test if else

--- Software/button.py
+++ Software/button.py
@@ -30,22 +30,64 @@ def button6(channel):
     print("6\n")
     time.sleep(0.1)
 
+GPIO.setwarnings(False)
 
-GPIO.setwarnings(False) # Ignore warning for now
+button1 = 38
+button2 = 36
+button3 = 34
+button4 = 32
+button5 = 30
+button6 = 28
 
-GPIO.setmode(GPIO.BCM) # Use physical pin numbering
-GPIO.setup(0, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(button1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(button2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(button3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(button4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(button5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(button6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+'''
 GPIO.add_event_detect(0,GPIO.RISING,callback=button1, bouncetime=500)
 GPIO.add_event_detect(5,GPIO.RISING,callback=button2, bouncetime=500)
 GPIO.add_event_detect(6,GPIO.RISING,callback=button3, bouncetime=500)
 GPIO.add_event_detect(13,GPIO.RISING,callback=button4, bouncetime=500)
 GPIO.add_event_detect(19,GPIO.RISING,callback=button5, bouncetime=500)
 GPIO.add_event_detect(26,GPIO.RISING,callback=button6, bouncetime=500)
+'''
+while True:
+    buttonState1 = GPIO.input(button1)
+    buttonState2 = GPIO.input(button2)
+    buttonState3 = GPIO.input(button3)
+    buttonState4 = GPIO.input(button4)
+    buttonState5 = GPIO.input(button5)
+    buttonState6 = GPIO.input(button6)
+
+    if (buttonState1):
+        button1()
+        time.sleep(0.1)
+
+    elif (buttonState2):
+        button2()
+        time.sleep(0.1)
+
+    elif (buttonState3):
+        button3()
+        time.sleep(0.1)
+
+    elif (buttonState4):
+        button4()
+        time.sleep(0.1)
+
+    elif (buttonState5):
+        button5()
+        time.sleep(0.1)
+
+    elif (buttonState6):
+        button6()
+        time.sleep(0.1)
+        
+    else:
+        continue
 
 input()
