785005c76cf86550b4bda8e45b3453c8a91c8906
Yetkin Akyüz <yetkin.akyz@gmail.com>
2020-11-19 14:34:10 +0300

    new file main.py

--- Software/button.py
+++ Software/button.py
@@ -1,35 +1,6 @@
 import time
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
-def button1(c):
-    print("1\n")
-    time.sleep(0.1)
-
-
-def button2(c):
-    print("2\n")
-    time.sleep(0.1)
-
-
-def button3(c):
-    print("3\n")
-    time.sleep(0.1)
-
-
-def button4(c):
-    print("4\n")
-    time.sleep(0.1)
-
-
-def button5(c):
-    print("5\n")
-    time.sleep(0.1)
-
-
-def button6(c):
-    print("6\n")
-    time.sleep(0.1)
-
 GPIO.setwarnings(False)
 
 button1 = 0
@@ -47,12 +18,51 @@ GPIO.setup(button4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(button5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(button6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+'''
+GPIO.add_event_detect(0,GPIO.RISING,callback=button1, bouncetime=500)
+GPIO.add_event_detect(5,GPIO.RISING,callback=button2, bouncetime=500)
+GPIO.add_event_detect(6,GPIO.RISING,callback=button3, bouncetime=500)
+GPIO.add_event_detect(13,GPIO.RISING,callback=button4, bouncetime=500)
+GPIO.add_event_detect(19,GPIO.RISING,callback=button5, bouncetime=500)
+GPIO.add_event_detect(26,GPIO.RISING,callback=button6, bouncetime=500)
+'''
+while True:
+    buttonState1 = GPIO.input(button1)
+    buttonState2 = GPIO.input(button2)
+    buttonState3 = GPIO.input(button3)
+    buttonState4 = GPIO.input(button4)
+    buttonState5 = GPIO.input(button5)
+    buttonState6 = GPIO.input(button6)
 
-GPIO.add_event_detect(0,GPIO.BOTH,callback=button1, bouncetime=500)
-GPIO.add_event_detect(5,GPIO.BOTH,callback=button2, bouncetime=500)
-GPIO.add_event_detect(6,GPIO.BOTH,callback=button3, bouncetime=500)
-GPIO.add_event_detect(13,GPIO.BOTH,callback=button4, bouncetime=500)
-GPIO.add_event_detect(19,GPIO.BOTH,callback=button5, bouncetime=500)
-GPIO.add_event_detect(26,GPIO.BOTH,callback=button6, bouncetime=500)
+    if (not buttonState1):
+
+        print("1\n")
+        time.sleep(0.5)
+
+    elif (buttonState2):
+
+        print("2\n")
+        time.sleep(0.5)
+
+    elif (buttonState3):
+
+        print("3\n")
+        time.sleep(0.5)
+
+    elif (buttonState4):
+
+        print("4\n")
+        time.sleep(0.5)
+
+    elif (buttonState5):
+        print("5\n")
+        time.sleep(0.5)
+
+    elif (buttonState6):
+        print("6\n")
+        time.sleep(0.5)
+
+    else:
+        continue
 
 input()
