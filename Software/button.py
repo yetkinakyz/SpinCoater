613d0f9bdb0b16179a09e6ed5605b0ae5981b092
Yetkin Akyüz <yetkin.akyz@gmail.com>
2020-11-19 14:05:51 +0300

    button

--- Software/button.py
+++ Software/button.py
@@ -47,51 +47,12 @@ GPIO.setup(button4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(button5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(button6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-'''
-GPIO.add_event_detect(0,GPIO.RISING,callback=button1, bouncetime=500)
-GPIO.add_event_detect(5,GPIO.RISING,callback=button2, bouncetime=500)
-GPIO.add_event_detect(6,GPIO.RISING,callback=button3, bouncetime=500)
-GPIO.add_event_detect(13,GPIO.RISING,callback=button4, bouncetime=500)
-GPIO.add_event_detect(19,GPIO.RISING,callback=button5, bouncetime=500)
-GPIO.add_event_detect(26,GPIO.RISING,callback=button6, bouncetime=500)
-'''
-while True:
-    buttonState1 = GPIO.input(button1)
-    buttonState2 = GPIO.input(button2)
-    buttonState3 = GPIO.input(button3)
-    buttonState4 = GPIO.input(button4)
-    buttonState5 = GPIO.input(button5)
-    buttonState6 = GPIO.input(button6)
 
-    if (not buttonState1):
-
-        print("1\n")
-        time.sleep(0.5)
-
-    elif (buttonState2):
-
-        print("2\n")
-        time.sleep(0.5)
-
-    elif (buttonState3):
-
-        print("3\n")
-        time.sleep(0.5)
-
-    elif (buttonState4):
-
-        print("4\n")
-        time.sleep(0.5)
-
-    elif (buttonState5):
-        print("5\n")
-        time.sleep(0.5)
-
-    elif (buttonState6):
-        print("6\n")
-        time.sleep(0.5)
-
-    else:
-        continue
+GPIO.add_event_detect(0,GPIO.BOTH,callback=button1, bouncetime=500)
+GPIO.add_event_detect(5,GPIO.BOTH,callback=button2, bouncetime=500)
+GPIO.add_event_detect(6,GPIO.BOTH,callback=button3, bouncetime=500)
+GPIO.add_event_detect(13,GPIO.BOTH,callback=button4, bouncetime=500)
+GPIO.add_event_detect(19,GPIO.BOTH,callback=button5, bouncetime=500)
+GPIO.add_event_detect(26,GPIO.BOTH,callback=button6, bouncetime=500)
 
 input()
