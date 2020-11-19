4dd812d66e1b4104dfe63881deafc3cc5c85e77e
Yetkin Akyüz <yetkin.akyz@gmail.com>
2020-11-19 18:08:45 +0300

    menu

--- Software/main.py
+++ Software/main.py
@@ -41,8 +41,8 @@ buttons = [ None,
 
 ## MENU
 menuPosition = 0
-mainMenu = [" SET PROGRAM ", "       TEST ", "        INFO "]
-menuAscii = [chr(124) + chr(62), chr(124) + chr(60)+chr(62), chr(60) + chr(174)] 
+mainMenu = ["  SET PROGRAM ", "       TEST ", "        INFO "]
+menuAscii = [chr(124) + chr(62), chr(60) + chr(124) + chr(62), chr(60) + chr(124)] 
 
 while True:
 
