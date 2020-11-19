440cd9bb3b47bf1fa93d61a80df8cb9aa5f69a03
Yetkin Akyüz <yetkin.akyz@gmail.com>
2020-11-19 23:18:49 +0300

    info

--- Software/main.py
+++ Software/main.py
@@ -96,6 +96,26 @@ while True:
                 time.sleep(2)
 
                 break
+        
+        elif menuPosition == 2:
+            while not GPIO.input(button5):
+                display.lcd_clear()
+
+                display.lcd_display_string("SPIN COATER     ", 1) #PRINT LINE 1
+                display.lcd_display_string("         PROJECT", 2) #PRINT LINE 1
+                time.sleep(2)
+
+                display.lcd_display_string("MADE BY", 1) #PRINT LINE 1
+                display.lcd_display_string("    YETKIN AKYUZ", 2) #PRINT LINE 1
+                time.sleep(2)
+
+                display.lcd_display_string("Version: 1.0", 1) #PRINT LINE 1
+                display.lcd_display_string("Date: 18.11.2020", 2) #PRINT LINE 1
+                time.sleep(2)
+
+                display.lcd_display_string("Website:        ", 1) #PRINT LINE 1
+                display.lcd_display_string("yetkinakyuz.com ", 2) #PRINT LINE 1
+                time.sleep(2)
 
         else:
             continue
