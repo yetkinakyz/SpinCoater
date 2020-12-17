03825bdfd16e3096e8e1fb215cd15f4394b17cf1
Yetkin Akyüz <yetkin.akyz@gmail.com>
2020-12-17 15:21:29 +0300

    new sample values

--- Software/RpmControl.py
+++ Software/RpmControl.py
@@ -252,22 +252,22 @@ def SetSample(rpm):
     global sample
 
     if rpm <= 500:
-        sample = 10
-
-    elif rpm > 500 and rpm <= 1500:
-        sample = 20
-
-    elif rpm > 1500 and rpm <= 2500:
         sample = 30
 
+    elif rpm > 500 and rpm <= 1500:
+        sample = 45
+
+    elif rpm > 1500 and rpm <= 2500:
+        sample = 60
+
     elif rpm > 2500 and rpm <= 3500:
-        sample = 40
+        sample = 75
 
     elif rpm > 3500 and rpm <= 4500:
-        sample = 50
+        sample = 90
 
     elif rpm > 4500:
-        sample = 60
+        sample = 120
     
     return sample
 
